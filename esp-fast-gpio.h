8bb8fe7eee5c6c9a82d9817c0b39af1fc6eae4b3
Michael Hill <bitfixer@bitfixer.com>
2025-05-21 16:07:02 -0700

    make functions inline

--- esp-fast-gpio.h
+++ esp-fast-gpio.h
@@ -3,12 +3,40 @@
 
 #pragma once
 #include <inttypes.h>
+#include <hal/gpio_hal.h>
+#include <driver/gpio.h>
 
 namespace EspFastGpio {
+
+    extern gpio_hal_context_t _gpio_hal;
+    extern gpio_dev_t *dev;
+
+    extern volatile uint32_t* gpio_low_set_reg;
+    extern volatile uint32_t* gpio_low_clear_reg;
+    extern volatile uint32_t* gpio_low_enable_set_reg;
+    extern volatile uint32_t* gpio_low_enable_clear_reg;
+    extern volatile uint32_t* gpio_low_in;
+    extern uint32_t gpio_masks[32];
+
     void init();
-    void setInput(int pin);
-    void setOutput(int pin);
-    void setHigh(int pin);
-    void setLow(int pin);
-    uint32_t get(int pin);
+    
+    inline void setInput(int pin) {
+        *gpio_low_enable_clear_reg = gpio_masks[pin];
+    }
+
+    inline void setOutput(int pin) {
+        *gpio_low_enable_set_reg = gpio_masks[pin];
+    }
+
+    inline void setHigh(int pin) {
+        *gpio_low_set_reg = gpio_masks[pin];
+    }
+
+    inline void setLow(int pin) {
+        *gpio_low_clear_reg = gpio_masks[pin];
+    }
+
+    inline uint32_t get(int pin) {
+        return (*gpio_low_in >> pin) & 0x1;
+    }
 }
